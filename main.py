0a8c63e62aa86a2c3a084d51b0d082c3317679e1
David810209 <a1050120bc@gmail.com>
2024-05-27 19:17:42 +0800

    first

---
+++ main.py
@@ -0,0 +1,45 @@
+from openai import OpenAI
+from langchain_openai import ChatOpenAI
+from dotenv import load_dotenv
+import fitz 
+load_dotenv()
+
+def extract_text_from_pdf(pdf_path):
+    doc = fitz.open(pdf_path)
+    text = ""
+    for page in doc:
+        text += page.get_text()
+    doc.close()
+    return text
+llm = ChatOpenAI()
+pdf_text = extract_text_from_pdf("test.pdf")
+user_question = input("請輸入你的問題: ")
+combined_input = pdf_text + "\n這段文字的大綱" 
+response = llm.invoke(combined_input).content
+print("AI 回應:", response)
+# llm = ChatOpenAI()
+# while(True):
+#     print("input:")
+#     question = input()
+#     print("\noutput:")
+#     print(llm.invoke(question).content)
+#     if question  == "0":
+#         break
+
+#llm.invoke("Hello, world!")
+
+# client = OpenAI()
+# print("input:")
+# question = input()
+# completion = client.chat.completions.create(
+#   model="gpt-3.5-turbo",
+#   messages=[
+#     {"role": "system", "content": "You are a poetic assistant, skilled in explaining complex programming concepts with creative flair."},
+#     {"role": "user", "content": question }
+#   ]
+# )
+# #"Compose a poem that explains the concept of recursion in programming."
+# print("\noutput:")
+# print(completion.choices[0].message.content)
+
+
