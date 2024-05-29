f17bfaa41fcc7021e4ad537e6326b0ac5591f12e
David810209 <a1050120bc@gmail.com>
2024-05-29 22:35:53 +0800

    test

--- redis_db.py
+++ redis_db.py
@@ -2,16 +2,19 @@ import redis
 
 class RedisUser():
     def __init__(self,host,port,password) :
-        self.rds = redis.Redis(host=host,port = port ,password=password)
+        self.rds = redis.Redis(host=host,
+                               port = port ,
+                               password=password,
+                            decode_responses=True)
     
     def set_user_name(self,user_id,user_name):
         self.rds.hmset(f'user:{user_id}',{
             'user_name':user_name,
-            "notion_api_key":None,
-            "notion_db_id":None,
-            "pinecone_index_name":None,
-            "pinecone_environment":None,
-            "pinecone_api_key":None
+            "notion_api_key":"",
+            "notion_db_id":"",
+            "pinecone_index_name":"",
+            "pinecone_environment":"",
+            "pinecone_api_key":""
         })
     def get_user_name(self,user_id):
         return self.rds.hget(f'user:{user_id}','user_name')
